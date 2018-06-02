d012c7a48e10caa1a4c9bc497ef1235274c8945d
Jesper Friis <jesper.friis@sintef.no>
2018-06-02 19:17:41 +0200

    Improved generated vocabulary.

--- python/emmo/tests/test_vocabulary.py
+++ python/emmo/tests/test_vocabulary.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import subprocess
+import time
 
 # Add emmo to sys path
 thisdir = os.path.abspath(os.path.dirname(__file__))
@@ -27,4 +28,12 @@ with open('emmo.html', 'w') as f:
 # Markdown vocabulary converted to html with pandoc
 with open('emmodoc.md', 'w') as f:
     f.write(emmo.get_vocabulary(template='markdown') + '\n')
-subprocess.call(['pandoc', '-s', '--toc', 'emmodoc.md', '-o', 'emmodoc.html'])
+subtitle = 'Generated from <a href="%s">%s</a>' % (emmo.base_iri, emmo.base_iri)
+subprocess.call(['pandoc', 'emmodoc.md', 'emmodoc-meta.yaml',
+                 '--standalone', '--self-contained', '--toc', '--toc-depth=2',
+                 '--variable=date:%s' % time.strftime('%B %d, %Y'),
+                 '--variable=subtitle:%s' % subtitle,
+                 '--to=html5',
+                 '--css=emmodoc.css',
+                 '--template=emmodoc-template.html',
+                 '--output=emmodoc.html'])
