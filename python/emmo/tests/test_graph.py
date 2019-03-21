50fb51e054ef8b90ebed46c914ceed0a93023a4b
Jesper Friis <jesper.friis@sintef.no>
2019-03-21 22:18:21 +0100

    Fixed tests/test_graph.py

--- python/emmo/tests/test_graph.py
+++ python/emmo/tests/test_graph.py
@@ -11,14 +11,14 @@ from emmo import get_ontology
 ##########################################################
 ## EMMO base
 ##########################################################
-emmo = get_ontology('emmo.owl')
+emmo = get_ontology()
 emmo.load()
 
 graph = emmo.get_dot_graph(relations=True, style='uml')
 graph.write_svg('graph-noreason.svg')
 graph.write_pdf('graph-noreason.pdf')
 
-emmo.sync_reasoner()
+#emmo.sync_reasoner()
 
 #graph = emmo.get_dot_graph(relations=True, style='uml')
 relations = (
@@ -31,11 +31,11 @@ graph = emmo.get_dot_graph(relations=relations, style='uml')
 graph.write_svg('graph.svg')
 graph.write_pdf('graph.pdf')
 
-entity_graph = emmo.get_dot_graph('entity')
+entity_graph = emmo.get_dot_graph('emmo')
 entity_graph.write_pdf('entity_graph.pdf')
 entity_graph.write_svg('entity_graph.svg')
 
-substrate_graph = emmo.get_dot_graph('substrate', relations=True,
+substrate_graph = emmo.get_dot_graph('item', relations=True,
                                      leafs=('spacetime'), parents='item',
                                      style='uml')
 substrate_graph.write_pdf('substrate_graph.pdf')
@@ -50,14 +50,3 @@ property_graph.write_pdf('property_graph.pdf')
 #relations_graph = emmo.get_dot_relations_graph()
 relations_graph = emmo.get_dot_graph('relation')
 relations_graph.write_pdf('relation_graph.pdf')
-
-
-##########################################################
-## Material
-##########################################################
-material = get_ontology('emmo-material.owl')
-#material.name = 'material'
-material.load()
-material.sync_reasoner()
-material_graph = material.get_dot_graph()
-material_graph.write_pdf('material.pdf')
