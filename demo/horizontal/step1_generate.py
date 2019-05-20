3b587843648458ff1fb18963858fa799d7d7bd07
Jesper Friis <jesper.friis@sintef.no>
2019-05-21 00:13:32 +0200

    Separated the workflow into several scripts.  There is an issue with atoms.json that must be fixed

--- demo/horizontal/step1_generate.py
+++ demo/horizontal/step1_generate.py
@@ -1,4 +1,5 @@
 import os
+
 from emmo2meta import EMMO2Meta
 
 
@@ -7,6 +8,6 @@ ontopath = os.path.abspath(os.path.join(
 
 
 
-e = EMMO2Meta(ontology=ontopath, classes='ASE_Atoms')
+e = EMMO2Meta(ontology=ontopath, classes='ASE_Atoms', collid='ASE_Atoms')
 e.save('json', 'horizontal.json', 'mode=w')
 onto = e.onto
