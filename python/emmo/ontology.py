826b9579a15551c7859f46efef32b64e2b250076
Jesper Friis <jesper.friis@sintef.no>
2019-05-28 21:43:36 +0200

    Removed the need to explicitly provide a label when extending an ontology using the Python API.  This greatly reduces the number of bugs introduced.

--- python/emmo/ontology.py
+++ python/emmo/ontology.py
@@ -135,6 +135,15 @@ class Ontology(owlready2.Ontology, OntoGraph, OntoVocab):
         }
         if label in d:
             return d[label]
+        # Check whether `label` matches a Python class name of any category
+        l = [cls for cls in itertools.chain.from_iterable(
+            getattr(self, category)() for category in categories)
+             if hasattr(cls, '__name__') and cls.__name__ == label]
+        if len(l) == 1:
+            return l[0]
+        elif len(l) > 1:
+            raise NoSuchLabelError('There is more than one Python class with '
+                                   'name %r' % label)
         # Check imported ontologies
         for onto in self.imported_ontologies:
             onto.__class__ = self.__class__  # magically change type of onto
@@ -142,6 +151,15 @@ class Ontology(owlready2.Ontology, OntoGraph, OntoVocab):
                 return onto.get_by_label(label)
             except NoSuchLabelError:
                 pass
+        # Fallback to check whether we have a class in the current or any
+        # of the imported ontologies whos name matches `label`
+        #for onto in [self] + self.imported_ontologies:
+        #    l = [cls for cls in onto.classes() if cls.__name__ == label]
+        #    if len(l) == 1:
+        #        return l[0]
+        #    elif len(l) > 1:
+        #        raise NoSuchLabelError('There is more than one class with '
+        #                               'name %r' % label)
         # Label cannot be found
         raise NoSuchLabelError('Ontology "%s" has no such label: %s' % (
             self.name, label))
@@ -151,13 +169,46 @@ class Ontology(owlready2.Ontology, OntoGraph, OntoVocab):
         matching labels.
         """
         return [entity for entity in
-                itertools.chain(*(getattr(self, c)() for c in categories))
-                if label in entity.label]
+                itertools.chain.from_iterable(
+                    getattr(self, c)() for c in categories)
+                if hasattr(entity, 'label') and label in entity.label]
 
     def sync_reasoner(self):
         """Update current ontology by running the HermiT reasoner."""
         with self:
             owlready2.sync_reasoner()
+
+    def sync_attributes(self, sync_imported=False):
+        """Call method is intended to be called after you have added new
+        classes (typically via Python).
+
+        If a class, object property or individual in the current
+        ontology has no label, the name of the corresponding Python class
+        will be assigned as label.
+
+        If a class, object property or individual has no comment, it will
+        be assigned the docstring of the corresponding Python class.
+
+        If `sync_imported` is true, all imported ontologies are also
+        updated.
+        """
+        for cls in itertools.chain(self.classes(), self.object_properties(),
+                                   self.individuals()):
+            if not cls.label:
+                cls.label.append(cls.__name__)
+            if not cls.comment and cls.__doc__:
+                cls.comment.append(cls.__doc__)
+        if sync_imported:
+            for onto in self.imported_ontologies:
+                onto.sync_attributes()
+        # FIXME - optionally, consider to also update the class names.
+        # Possible options could be:
+        #   - do not change names (defalt)
+        #   - set name to ``prefix + "_" + uuid`` where `prefix` is any
+        #     string (e.g. "EMMO") and `uuid` is an uuid.  This is the
+        #     default for EMMO.
+        #   - set names to the name of the corresponding Python class
+        #   - set names equal to labels
 
     def get_relations(self):
         """Returns a generator for all relations."""
