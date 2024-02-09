26818a099400ff2c35e83df53f9ac00b7de54c52
hothello <roscioni@gmail.com>
2024-02-09 12:36:21 +0000

    Triple quotes added back, and removed from the fixes.sh script.

--- scripts/fixes.sh
+++ scripts/fixes.sh
@@ -2,8 +2,6 @@
 
 for i in $(find . -name "*ttl");
 do
-  # Change triples double-quotes (""") to single ones (").
-  sed -i 's/"""/"/g' "$i"
   # Convert the IRI with the format EMMO_hex-hex-hex-hex-hex.
   sed -i -E 's/EMMO_([0-9a-f]+)-([0-9a-f]+)-([0-9a-f]+)-([0-9a-f]+)-([0-9a-f]+)/EMMO_\1_\2_\3_\4_\5/g' "$i";
   # Convert the IRI without the EMMO_ prefix.
