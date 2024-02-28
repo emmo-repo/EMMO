74a146e17059144987c08f678c22ef84d50b65cc
Emanuele Ghedini <emanuele.ghedini@gmail.com>
2024-02-28 11:17:29 +0100

    w3c prefix and beta7

--- doc/emmodoc/mkfigs.py
+++ doc/emmodoc/mkfigs.py
@@ -9,7 +9,7 @@ from emmo.graph import (plot_modules, get_module_dependencies,
 
 
 # Plot module dependencies
-iri = 'http://emmo.info/emmo/1.0.0-beta6'
+iri = 'https://w3id.org/emmo/1.0.0-beta7'
 emmo = get_ontology(iri)
 emmo.load()
 modules = get_module_dependencies(emmo)
