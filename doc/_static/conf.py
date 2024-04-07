5e13647e2c10e95bdc997f5c5fd13052081ee10f
Jesper Friis <jesper.friis@sintef.no>
2024-04-07 23:30:45 +0200

    Installed missing dependencies

--- doc/_static/conf.py
+++ doc/_static/conf.py
@@ -62,14 +62,14 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['doc/_static']
-html_css_files = ['doc/_static/custom.css']
+html_static_path = ['../doc/_static']
+html_css_files = ['../doc/_static/custom.css']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'doc/emmo-logo.png'
+html_logo = '../doc/emmo-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_flavicon = 'doc/emmo-logo.ico'
+html_flavicon = '../doc/figs/emmo-logo.ico'
