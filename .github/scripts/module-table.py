1c30a8936cde38c473f5786cbc35f0f6d3a5f862
Jesper Friis <jesper.friis@sintef.no>
2026-01-18 11:44:52 +0100

    Added HUME ontology IRIs to module table

--- .github/scripts/module-table.py
+++ .github/scripts/module-table.py
@@ -1,25 +1,21 @@
 import subprocess
 
-level_files = [
-    "foundation/foundation.ttl",
-    "perspectives/perspectives.ttl",
-    "reference/reference.ttl",
-    "disciplines/disciplines.ttl",
-]
-
-sorting_order = [
+levels = [
     "foundation",
     "perspectives",
     "reference",
     "disciplines",
 ]
+level_files = [f"{level}/{level}.ttl" for level in levels]
+
 
 def sorter(key):
     """Function defining module sorting order."""
-    for i, k in enumerate(sorting_order):
+    for i, k in enumerate(levels):
         if key.startswith(k):
             return f"{i}{key}"
     return key
+
 
 files = subprocess.check_output(
     ["git", "ls-files", "*.ttl"]
@@ -30,17 +26,20 @@ module_files = sorted(set(files).difference(level_files), key=sorter)
 # TODO: Consider to add a forth column containing the abstract (or
 # dcterms:description) or all the ontology modules.
 
-widths = [0, 0, 0]  # initial column widths
+widths = [0, 0, 0, 0]  # initial column widths
 table = []
 for filename in module_files:
     basename = filename[:-4]
     module = basename.split("/")[-1]
     level = basename.split("/")[0]
-    if level not in sorting_order:
+    if level not in levels:
         level = ""
     iri = f"https://w3id.org/emmo/{basename}"
+    humeiri = f"https://w3id.org/emmo/hume/{basename}"
+    ontology_iri = f"[{iri}]({iri})"
+    hume_ontology_iri = f"[{humeiri}]({humeiri})" if level else ""
 
-    table.append([module, level, f"[{iri}]({iri})"])
+    table.append([module, level, ontology_iri, hume_ontology_iri])
 
 for row in table:
     for i, cell in enumerate(row):
@@ -48,7 +47,7 @@ for row in table:
             widths[i] = len(cell)
 
 # Insert table header
-table.insert(0, ["Module", "Level", "Ontology IRI"])
+table.insert(0, ["Module", "Level", "Ontology IRI", "HUME ontology IRI"])
 table.insert(1, ["-" * w for w in widths])
 
 lines = []
